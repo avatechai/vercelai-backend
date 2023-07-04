29eba8d6db5b932b98f81e57bf2736ee43e6ef49
Edwin <ew20128@gmail.com>
2023-07-04 11:05:16 +0800

    remove auth from api chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,9 @@
-import { kv } from '@vercel/kv'
+// import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
+// import { auth } from '@/auth'
+// import { nanoid } from '@/lib/utils'
 
 export const runtime = 'edge'
 
@@ -16,13 +16,13 @@ const openai = new OpenAIApi(configuration)
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+  // const userId = (await auth())?.user.id
 
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
+  // if (!userId) {
+  //   return new Response('Unauthorized', {
+  //     status: 401
+  //   })
+  // }
 
   if (previewToken) {
     configuration.apiKey = previewToken
@@ -36,31 +36,31 @@ export async function POST(req: Request) {
   })
 
   const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
+    // async onCompletion(completion) {
+    //   const title = json.messages[0].content.substring(0, 100)
+    //   const id = json.id ?? nanoid()
+    //   const createdAt = Date.now()
+    //   const path = `/chat/${id}`
+    //   const payload = {
+    //     id,
+    //     title,
+    //     userId,
+    //     createdAt,
+    //     path,
+    //     messages: [
+    //       ...messages,
+    //       {
+    //         content: completion,
+    //         role: 'assistant'
+    //       }
+    //     ]
+    //   }
+    //   await kv.hmset(`chat:${id}`, payload)
+    //   await kv.zadd(`user:chat:${userId}`, {
+    //     score: createdAt,
+    //     member: `chat:${id}`
+    //   })
+    // }
   })
 
   return new StreamingTextResponse(stream)
