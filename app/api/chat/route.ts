4f168dd3c424e45f246f00177a34166e44faf9cb
Edwin <ew20128@gmail.com>
2023-07-04 15:24:20 +0800

    chore: remove all frontend page

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,55 +16,14 @@ export async function POST(req: Request) {
   const json = await req.json()
   const { messages, apiKey } = json
 
-  // const userId = (await auth())?.user.id
-  // const configuration = new Configuration({
-  //   apiKey: apiKey
-  // })
-
-  // console.log(apiKey)
-
-  // const openai = new OpenAIApi(configuration)
-  // if (!userId) {
-  //   return new Response('Unauthorized', {
-  //     status: 401
-  //   })
-  // }
-
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
     stream: true
   })
-  // console.log(await res.ok, res.body)
 
-  const stream = OpenAIStream(res, {
-    // async onCompletion(completion) {
-    //   const title = json.messages[0].content.substring(0, 100)
-    //   const id = json.id ?? nanoid()
-    //   const createdAt = Date.now()
-    //   const path = `/chat/${id}`
-    //   const payload = {
-    //     id,
-    //     title,
-    //     userId,
-    //     createdAt,
-    //     path,
-    //     messages: [
-    //       ...messages,
-    //       {
-    //         content: completion,
-    //         role: 'assistant'
-    //       }
-    //     ]
-    //   }
-    //   await kv.hmset(`chat:${id}`, payload)
-    //   await kv.zadd(`user:chat:${userId}`, {
-    //     score: createdAt,
-    //     member: `chat:${id}`
-    //   })
-    // }
-  })
+  const stream = OpenAIStream(res)
 
   return new StreamingTextResponse(stream, {
     headers: {
